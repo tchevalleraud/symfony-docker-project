f28a6f1a718740facf4e6ad37b6cbd298d842507
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2022-05-30 15:26:21 +0200

    Update #20220530-1526

--- app/config/swagger.php
+++ app/config/swagger.php
@@ -13,4 +13,26 @@
      *     url="http://localhost/api/v2/",
      *     description="Localhost"
      * )
+     *
+     * @OA\Schema(
+     *     schema="DateTime",
+     *     allOf={
+     *         @OA\Schema(
+     *             @OA\Property(property="date", type="string", format="date-time"),
+     *             @OA\Property(property="timezone_type", type="integer", default="3"),
+     *             @OA\Property(property="timezone", type="string", default="UTC")
+     *         )
+     *     }
+     * )
+     *
+     * @OA\Schema(
+     *     schema="Response200",
+     *     allOf={
+     *         @OA\Schema(
+     *             @OA\Property(property="code", type="integer", default="200"),
+     *             @OA\Property(property="datetime", type="array", @OA\Items(), ref="#/components/schemas/DateTime"),
+     *             @OA\Property(property="message", type="string", default="OK")
+     *         )
+     *     }
+     * )
      */
