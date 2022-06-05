e499bd3b56a780ef3225acd2e1dce4dec5feb8c6
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2022-06-05 09:11:06 +0200

    Add docusaurus

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,92 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Symfony Docker Project',
+  tagline: 'Custom framework for fast dev',
+  url: 'https://tchevalleraud.github.io',
+  baseUrl: '/symfony-docker-project/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/symfony.ico',
+  organizationName: 'tchevalleraud',
+  projectName: 'symfony-docker-project',
+  i18n: {
+    defaultLocale: 'fr',
+    locales: ['en', 'fr'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Symfony Docker Project',
+        logo: {
+          alt: 'Symfony Docker Project Logo',
+          src: 'img/symfony.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Documentation',
+          },
+          {
+            href: 'https://github.com/tchevalleraud/symfony-docker-project',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Documentation',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/tchevalleraud/symfony-docker-project',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Symfony Docker Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
