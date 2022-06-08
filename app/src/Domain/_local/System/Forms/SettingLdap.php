c5453cbf5c2ce02e03de1b0859013233e1d8892f
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2022-06-08 18:09:55 +0200

    Update #20220608-1809

--- app/src/Domain/_local/System/Forms/SettingLdap.php
+++ app/src/Domain/_local/System/Forms/SettingLdap.php
@@ -5,13 +5,30 @@
 
     class SettingLdap {
 
-        private $security_ldap_connections;
+        private $security_ldap_authentication_username;
+        private $security_ldap_authentication_password;
 
         public function __construct(SettingService $settingService){
+            $this->setSecurityLdapAuthenticationUsername($settingService->getSetting('security.ldap.authentication.username'));
+            $this->setSecurityLdapAuthenticationPassword($settingService->getSetting('security.ldap.authentication.password'));
         }
 
-        public function getSecurityLdapConnections(){
-            return $this->security_ldap_connections;
+        public function getSecurityLdapAuthenticationUsername(){
+            return $this->security_ldap_authentication_username;
+        }
+
+        public function setSecurityLdapAuthenticationUsername($security_ldap_authentication_username): self {
+            $this->security_ldap_authentication_username = $security_ldap_authentication_username;
+            return $this;
+        }
+
+        public function getSecurityLdapAuthenticationPassword(){
+            return $this->security_ldap_authentication_password;
+        }
+
+        public function setSecurityLdapAuthenticationPassword($security_ldap_authentication_password): self {
+            $this->security_ldap_authentication_password = $security_ldap_authentication_password;
+            return $this;
         }
 
     }
--- app/src/Infrastructure/Forms/AdminOffice/Authentication/LDAP/EditForm.php
+++ app/src/Infrastructure/Forms/AdminOffice/Authentication/LDAP/EditForm.php
@@ -2,7 +2,9 @@
     namespace App\Infrastructure\Forms\AdminOffice\Authentication\LDAP;
 
     use Symfony\Component\Form\AbstractType;
+    use Symfony\Component\Form\Extension\Core\Type\PasswordType;
     use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+    use Symfony\Component\Form\Extension\Core\Type\TextType;
     use Symfony\Component\Form\FormBuilderInterface;
 
     class EditForm extends AbstractType {
@@ -10,6 +12,10 @@
         public function buildForm(FormBuilderInterface $builder, array $options) {
             parent::buildForm($builder, $options);
             $builder
+                ->add('security_ldap_authentication_username', TextType::class)
+                ->add('security_ldap_authentication_password', PasswordType::class, [
+                    'always_empty'  => false
+                ])
                 ->add('Submit', SubmitType::class);
         }
 
