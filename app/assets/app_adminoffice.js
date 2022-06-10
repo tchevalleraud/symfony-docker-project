a55acb7216b2e1a39e2c0fd5e69d704146a08b94
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2022-06-10 17:16:23 +0200

    Update #20220610-1716

--- app/assets/app_adminoffice.js
+++ app/assets/app_adminoffice.js
@@ -2,16 +2,32 @@ import 'bootstrap';
 import * as bootstrap from 'bootstrap';
 
 var securityIdle;
+var securityIdleShow = false;
+var securityIdleReload = false;
 var securityIdleTimer = 0;
 var toastSecurityIdle = document.getElementById('securityIdle')
+var toastSecurityIdleRemaining = document.getElementById('securityIdleRemaining')
 
 function checkSecurityIdle(){
     securityIdleTimer++;
     if(securityIdleTimer >= (toastSecurityIdle.dataset.idle - 60)){
-        var toast = new bootstrap.Toast(toastSecurityIdle)
-        toast.show()
+        if(securityIdleShow == false){
+            var toast = new bootstrap.Toast(toastSecurityIdle, {
+                'delay' : 60000
+            })
+            toast.show()
+            securityIdleShow = true;
+        }
     }
-    console.log(securityIdleTimer);
+
+    if(securityIdleTimer > toastSecurityIdle.dataset.idle){
+        if(securityIdleReload == false){
+            return document.location.reload();
+        }
+    }
+
+    toastSecurityIdleRemaining.innerHTML = toastSecurityIdle.dataset.idle - securityIdleTimer;
+
     securityIdle = setTimeout(checkSecurityIdle, 1000);
 }
 
