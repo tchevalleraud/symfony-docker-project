1372560468f7eb2de442b97a1657c6916a433aed
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2022-06-14 11:26:35 +0200

    Update #20220614-1126

--- app/tests/PantherTestCase/FrontOffice/DashboardTest.php
+++ app/tests/PantherTestCase/FrontOffice/DashboardTest.php
@@ -5,8 +5,16 @@
 
     class DashboardTest extends PantherTestCase {
 
+        public function test_EN_Index(){
+            $this->client->request('GET', '/en/dashboard.html');
+            $this->assertPageTitleContains('Symfony Docker Project');
+            $this->assertSelectorTextContains('h1', 'FrontOffice/Dashboard:index');
+        }
+
         public function test_FR_Index(){
-            $this->client->request('GET', '/');
+            $this->client->request('GET', '/fr/dashboard.html');
+            $this->assertPageTitleContains('Symfony Docker Project');
+            $this->assertSelectorTextContains('h1', 'FrontOffice/Dashboard:index');
         }
 
     }
--- app/tests/WebTestCase/FrontOffice/DashboardTest.php
+++ app/tests/WebTestCase/FrontOffice/DashboardTest.php
@@ -6,14 +6,14 @@
 
     class DashboardTest extends WebTestCase {
 
-        public function test_FR_Index(){
-            $crawler = $this->client->request('GET', '/fr/dashboard.html');
+        public function test_EN_Index(){
+            $crawler = $this->client->request('GET', '/en/dashboard.html');
             $this->assertResponseStatusCodeSame(Response::HTTP_OK);
             $this->assertEquals('Symfony Docker Project', $crawler->filter('title')->text());
         }
 
-        public function test_EN_Index(){
-            $crawler = $this->client->request('GET', '/en/dashboard.html');
+        public function test_FR_Index(){
+            $crawler = $this->client->request('GET', '/fr/dashboard.html');
             $this->assertResponseStatusCodeSame(Response::HTTP_OK);
             $this->assertEquals('Symfony Docker Project', $crawler->filter('title')->text());
         }
